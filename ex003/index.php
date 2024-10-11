cb15f6916ae7031bc00b5def09c85acf160a3608
Gabriel Cruces Dias de Oliveira <141298600+gabrielcruces@users.noreply.github.com>
2024-10-11 02:18:41 -0300

    subir arquivos

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivo</h1>
+    <?php 
+    
+    ?>
+    
+  
+// $num = 300;
+// echo "o valor da variavel é $num";
+
+
+  
+  
+</body>
+</html>
